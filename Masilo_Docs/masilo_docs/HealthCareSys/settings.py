bab81ae28f56d0ff5f000beae45c1f4e8827f018
morton <mortondala1@gmail.com>
2019-11-18 15:39:59 +0200

    added Gunicorn

--- Masilo_Docs/masilo_docs/HealthCareSys/settings.py
+++ Masilo_Docs/masilo_docs/HealthCareSys/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 from .SECRET import SECRET_KEY, DEBUG, EMAIL, PASSWORD
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,6 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
 
 ALLOWED_HOSTS = []
 
